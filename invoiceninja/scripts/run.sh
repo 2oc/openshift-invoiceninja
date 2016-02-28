878c5cb0741e69624ecb8ba42ce39e44758b5934
Joeri van Dooren <ure@moreorless.be>
2016-02-28 20:31:23 +0100

    ninja

--- invoiceninja/scripts/run.sh
+++ invoiceninja/scripts/run.sh
@@ -7,3 +7,10 @@ echo "invoiceninja:x:`id -u`:0:invoiceninja:/:/sbin/nologin" >> /etc/passwd
 echo "[${STAMP}] Starting daemon..."
 # run apache httpd daemon
 httpd -D FOREGROUND
+
+
+# ENV DB_HOST mysql
+# ENV DB_DATABASE ninja
+# ENV APP_KEY SomeRandomString
+# ENV LOG errorlog
+# ENV APP_DEBUG 0
