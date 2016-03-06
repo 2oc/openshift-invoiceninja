12083e4dc1d97ca6daaa0938c0d56a07ef6df38f
uRe <joeri@weepee.org>
2016-03-06 21:07:44 +0100

    test

--- invoiceninja/scripts/run.sh
+++ invoiceninja/scripts/run.sh
@@ -2,10 +2,8 @@
 
 STAMP=$(date)
 
-echo "invoiceninja:x:`id -u`:0:invoiceninja:/:/sbin/nologin" >> /etc/passwd
-
 echo "[${STAMP}] Starting hourly ..."
-/app/hourly/invoiceninja &
+#/app/hourly/invoiceninja &
 
 echo "[${STAMP}] Starting httpd daemon..."
 # run apache httpd daemon
